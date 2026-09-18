9aebf6b7c619c1f4f2977cb4354e9d7b06b90f31
Ahmad Shahzeb <ahmadshahzeb471@gmail.com>
2026-09-18 12:31:17 +0500

    lab 2

--- Lab 2  AI/task_3.py
+++ Lab 2  AI/task_3.py
@@ -15,3 +15,36 @@ def fun(name):
     print("Hello" +" "+  name)
 
 fun("Ahmad")
+
+# Default parameters
+
+def default(name="Ahmad"):
+    print("Hello" +" "+  name)
+
+default()
+default("Hello")
+
+# Passing Lists
+
+def lists(count):
+    for x in count:
+        print(x)
+
+
+list = ["first", "second", "third"]
+lists(list)
+
+# Return values
+
+def add(a, b):
+    return a + b
+
+result = add(5, 3)
+print(result)
+
+# keyword arguments
+
+def keyword(name, age):
+    print(f"Hello {name}, your age is {age}")
+
+keyword(name="Ahmad", age=21)   
