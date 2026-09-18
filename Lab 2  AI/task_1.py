6fedb832e01d3b2327a46ffd8b2dbf490a6cfba7
Ahmad Shahzeb <ahmadshahzeb471@gmail.com>
2026-09-18 12:03:30 +0500

    lab 2 task 1

---
+++ Lab 2  AI/task_1.py
@@ -0,0 +1,12 @@
+# While loop muti lines
+
+counter = 1
+while(counter <= 10):
+    print(f"count {counter}")
+    counter=counter+1
+
+
+# While loop single line
+
+x = 0
+while(x < 5): print(x);x=x+1
