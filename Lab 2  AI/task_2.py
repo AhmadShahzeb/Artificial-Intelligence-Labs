b657653f98e60ef143316f2901a748b4115a6833
Ahmad Shahzeb <ahmadshahzeb471@gmail.com>
2026-09-18 12:24:03 +0500

    2nd lab

---
+++ Lab 2  AI/task_2.py
@@ -0,0 +1,26 @@
+# For loop
+
+# List Iteration
+print("Lists\n")
+list = ["first", "second", "third"]
+for i in list:
+    print(i)
+
+
+# Tuple Iteration
+print("\nTuple\n")
+tup = ("first", "second", "third")
+for i in tup:
+    print(i)
+
+# String
+print("\nString\n")
+str = "Artificial"
+for i in str:
+    print(i)
+
+# Iterate by index
+print("\nindex iteration\n")
+length = len(list)
+for i in range(length):
+    print(list[i])
---
+++ Lab 2  AI/task_3.py
@@ -0,0 +1,17 @@
+# Functions
+
+# Calling the function
+
+def first():
+    print("Function calling")
+
+x = 1
+if x==1:
+    first()
+
+# Passing parameters
+
+def fun(name):
+    print("Hello" +" "+  name)
+
+fun("Ahmad")
